875bdbe48edcb2797f0695561dac26ed302c9d40
moonlitskies <smnch07@gmail.com>
2023-08-06 11:41:37 +0545

    initial form model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import{ FormBuilder } from '@angular/forms'
+import{ FormBuilder, FormArray, Validators } from '@angular/forms'
 
 
 @Component({
@@ -11,6 +11,29 @@ export class AppComponent{
   title = 'forms';
   constructor(private fb: FormBuilder){
   }
-  
 
+  formDetails = this.fb.group({
+    name: this.fb.group({
+      firstName:['', Validators.required ],
+      middleName:['', Validators.required],
+      LastName:['', Validators.required]
+    }),
+    phoneNo: ['', Validators.required],
+    email: ['', Validators.required],
+    dateOfBirth:['', Validators.required],
+    permanentAddress: this.fb.group({
+      provience:['', Validators.required],
+      district:['', Validators.required],
+      municipality:['', Validators.required],
+      villageName:['', Validators.required],
+      wardNo:['', Validators.required]
+    }),
+    eduQulification:['', Validators.required],
+    familyDetails: this.fb.group({
+      grandFatherName: ['', Validators.required],
+      fatherName:['', Validators.required],
+      motherName:['', Validators.required],
+    }),
+    photo: ['', Validators.required]
+  })
 }
